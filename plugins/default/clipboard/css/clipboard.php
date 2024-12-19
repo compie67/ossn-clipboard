c55c1c3e63f715cd966cc5763a37c2fed0311029
compie67 <compie67@gmail.com>
2024-12-19 11:23:14 +0100

    Update clipboard.php

--- plugins/default/clipboard/css/clipboard.php
+++ plugins/default/clipboard/css/clipboard.php
@@ -1,1 +1,63 @@
+header("Content-type: text/css; charset: UTF-8");
+?>
+.clipboard-activity-log a {
+    display: block;
+    margin: 15px 0;
+    padding: 10px 15px;
+    text-align: center;
+    border-radius: 5px;
+    text-decoration: none;
+    color: #fff;
+    background-color: #17a2b8; /* Info kleur */
+}
+
+.clipboard-activity-log a:hover {
+    background-color: #138496;
+}
+.clipboard {
+    position: fixed;
+    right: 0;
+    bottom: 0;
+    margin-right: 90px;
+    margin-bottom: 80px;
+}
+.menu-section-item-feedback:before {
+    content: "\f086" !important;
+}
+.clipboard-overview {
+    background-color: #e6ffe6; /* Lichte groene achtergrond */
+    padding: 20px;
+    border-radius: 10px;
+    border: 1px solid #d4d4d4;
+}
+.clipboard-post, .clipboard-blog {
+    margin-bottom: 15px;
+    padding: 10px;
+    border-bottom: 1px solid #c0c0c0;
+}
+.clipboard-photo img {
+    max-width: 100%;
+    height: auto;
+    margin-top: 10px;
+    border-radius: 5px;
+}
+.clipboard-comments, .clipboard-comment {
+    margin-top: 10px;
+    padding-left: 10px;
+    border-left: 3px solid #8bc34a;
+}
+.clipboard-download {
+    text-align: center;
+    margin-top: 20px;
+}
+.clipboard-download a {
+    background-color: #8bc34a;
+    color: #ffffff;
+    padding: 10px 20px;
+    border-radius: 5px;
+    text-decoration: none;
+}
+.clipboard-download a:hover {
+    background-color: #689f38;
+}
 
